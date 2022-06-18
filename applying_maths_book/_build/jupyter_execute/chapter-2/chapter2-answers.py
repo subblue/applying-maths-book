ee5824820ad0994b58dfba56d4d1956922e6480d
Tom Beddard <tom@subblue.com>
2022-06-18 15:47:05 +0100

    June 18 update

--- applying_maths_book/_build/jupyter_execute/chapter-2/chapter2-answers.py
+++ applying_maths_book/_build/jupyter_execute/chapter-2/chapter2-answers.py
@@ -374,6 +374,35 @@ C  = 1e-6
 # $$\displaystyle I=f^2(1+2\cos(\pi(h+k+l))$$
 # 
 # which means that the intensity is zero when $h+k+l $ is an odd integer and is typical for a body centred lattice.
+# 
+# ### Q29 Answer
+# 
+# The scattering factor is a complex number and converted to cosine / sine form is
+# 
+# $$\displaystyle F=\sum_i f_i\big( \cos(\pi (hu_i+kv_i+lw_i)) + i\sin(2\pi (\pi (hu_i+kv_i+lw_i))\big) $$
+# 
+# which can be put into the form 
+# 
+# $$\displaystyle F=A+iB$$
+# 
+# where $A$ is the summed cosine and $B$ the summed sine. The _intensity_ of the scattered diffraction is $F^*F$ or
+# 
+# $$\displaystyle  I=A^2+B^2$$
+# 
+# When the change $h,k,l\to -h,-k,-l$ is made the structure factor becomes,
+# 
+# $$\displaystyle F=\sum_i f_i( \cos(\pi (-hu_i-kv_i-lw_i)) + i\sin(2\pi (\pi (-hu_i-kv_i-lw_i))) $$
+# 
+# and as $\cos(-x) = \cos(x)$ and $\sin(-x)=-\sin(x)$ becomes
+# 
+# $$\displaystyle F=A-iB$$
+# 
+# and so the intensity becomes
+# 
+# $$\displaystyle I=A^2+B^2$$
+# 
+# The identical result for $(h,k,l)$ and $(-h,-k,-l)$  known as Friedel's law. Thus the diffraction image is centro-symmetric, even if the crystal is not. 
+# 
 
 # In[ ]:
 
